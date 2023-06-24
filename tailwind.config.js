4ffef0e0986b58e7d8021d00b4781659ea7b81d8
AfeefRazick <afeefrazickamir@gmail.com>
2023-06-24 10:19:50 +0530

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.{html,jsx}",
+    "./src/*.{html,jsx}",
+    "./src/**/**/*.{html,jsx}",
+    "./src/**/**/**/*.{html,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
