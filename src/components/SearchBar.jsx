466d5e6a76f6790e4d0be7ea44f53b014fe32b3f
BhanukaDev <bhanukadassanayake@gmail.com>
2023-06-27 19:47:41 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 import { BiSolidBus, BiSolidTrain, BiSearch } from 'react-icons/bi';
 import { Divider } from './Divider';
-export const SearchBar = ({ type }) => {
+export const SearchBar = ({ type, icon }) => {
   return (
     <div className='h-searchBar flex items-center z-searchBar w-searchBar sm:w-searchBarSmall md:w-searchBarMedium lg:w-searchBarLarge transition-width duration-150 rounded-full py-2 px-4 drop-shadow-lg bg-slate-50 absolute top-1 left-1/2 -translate-x-1/2'>
       {type === 'bus' ? (
         <BiSolidBus className='h-full w-[25px]' />
       ) : type === 'train' ? (
         <BiSolidTrain className='h-full w-[25px]' />
+      ) : type === 'custom' ? (
+        { icon }
       ) : (
         <BiSearch className='h-full w-[25px]' />
       )}
