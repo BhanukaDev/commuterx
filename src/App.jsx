45684712ee4dbf7d27c3df59d32aca6fd3574381
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-09 22:17:56 +0530

    map is now protected route

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,13 @@ export default function App() {
       <BrowserCheck />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/busmap" element={<MapPage />} />
-        <Route path="/trainmap" element={<MapPage />} />
+
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
 
         <Route element={<ProtectedRoutes />}>
+          <Route path="/busmap" element={<MapPage />} />
+          <Route path="/trainmap" element={<MapPage />} />
           <Route path="/profile" element={<ProfilePage />} />
         </Route>
 
