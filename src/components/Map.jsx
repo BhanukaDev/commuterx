777bc06ee721d6e0246b97e9fd7c543b08e984cd
BhanukaDev <bhanukadassanayake@gmail.com>
2023-08-09 15:16:10 +0530

    update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -29,7 +29,10 @@ export const Map = () => {
       options={mapOption}
       onLoad={onLoad}
     >
-      <Marker position={mapCenter}></Marker>
+      <Marker
+        position={mapCenter}
+        icon={"src/assets/person-solid.svg"}
+      ></Marker>
       <RecenterButton />
       {/* <Marker position={{ lat: 6.795506168321762, lng: 79.8557110778035 }} /> */}
     </GoogleMap>
