eb03675681324ca54158b3ecf445aa32d3b26c15
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-10 00:52:58 +0530

    major revamp to landing page, looks much better now

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,41 +3,48 @@
 import { Button } from "./Button"
 export const Hero = () => {
   return (
-    <div className="mt-16 flex h-[90vh] w-full flex-col items-center px-3 sm:px-[10%]">
-      <h1 className="my-10 text-center font-quicksand text-5xl font-bold text-black sm:text-5xl md:text-5xl lg:text-6xl">
+    <div className="flex h-screen w-full flex-col px-5 pt-16 sm:px-[6%]">
+      {/* <h1 className="my-10 text-center font-quicksand text-5xl font-bold text-black sm:text-5xl md:text-5xl lg:text-6xl">
         <span className="mr-2 font-lato sc0:max-sm:text-4xl">Welcome to </span>
         <span className="inline-block first-letter:text-accent">
           CommuterX!
         </span>
+      </h1> */}
+      <h1 className="mt-10 w-11/12 max-w-xs font-manrope text-[clamp(34px,5vw,54px)] font-bold leading-[1.05] text-black md:mt-16 md:max-w-sm lg:max-w-lg ">
+        The solution to your commuting problems
       </h1>
 
-      <p className="my-5 w-full text-center">
-        Stay on track with our live transit tracker. Whether you&#39;re a daily
+      <p className=" mt-5 w-full text-lg text-zinc-500 sm:mt-4 sm:text-xl md:mt-8 md:text-2xl">
+        Stay on track with our live transit tracker.
+      </p>
+      {/* Whether you&#39;re a daily
         commuter or an occasional traveler, we&#39;ve got you covered with
         real-time updates on the location of buses and trains around the
-        country.
-      </p>
+        country. */}
 
-      <p className="mb-8 w-full text-center">
+      {/* <p className="mb-8 w-full text-center">
         Our user-friendly interface and robust tracking system make it easier
         than ever to plan your journey and minimize waiting times.
-      </p>
+      </p> */}
+      <figure className="right-10 flex h-[calc(100%-64px)] w-1/3 max-w-[280px] items-center md:absolute lg:right-14">
+        <img className="" src="trainroutes.png" alt="" />
+      </figure>
 
-      <div className="flex justify-center ">
-        <img className="mx-5 h-20 w-20" src="./icons8-bus-64.png" alt="" />
-        <img className="mx-5 h-20 w-20" src="./icons8-train-64.png" alt="" />
+      <div className="flex md:mt-24">
+        <img className="mr-8 h-20 w-20" src="./icons8-bus-64.png" alt="" />
+        <img className="mr-5 h-20 w-20" src="./icons8-train-64.png" alt="" />
       </div>
 
-      <div className="my-5 flex justify-center">
+      <div className="my-5 flex ">
         <Button
-          className="mx-5 w-20"
+          className="mr-8 w-20"
           buttonStyle="btn-outline"
           linkpath="/busmap"
         >
           Bus
         </Button>
         <Button
-          className="mx-5 w-20"
+          className="mr-5 w-20"
           buttonStyle="btn-outline"
           linkpath="/trainmap"
         >
