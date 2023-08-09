a5452d8b7fc37d3922a06b14e3a44640517733d8
BhanukaDev <bhanukadassanayake@gmail.com>
2023-08-09 14:36:44 +0530

    u

--- src/Utils/database.js
+++ src/Utils/database.js
@@ -4,12 +4,19 @@ import { auth } from "./auth"
 
 export const db = getFirestore(app)
 
+export const addToUniteDB = (id, role) => {
+  setDoc(doc(db, "Unite", id), {
+    role: role,
+  })
+}
 export const addBusToDatabase = (data) => {
   console.log(data.uid)
   setDoc(doc(db, "Buses", data.uid), { ...data })
+  addToUniteDB(data.uid, "bus")
 }
 export const addTrainToDatabase = (data) => {
   setDoc(doc(db, "Trains", data.uid), { ...data })
+  addToUniteDB(data.uid, "train")
 }
 export const addCommuterToDatabase = () => {
   const user = auth.currentUser
@@ -18,4 +25,5 @@ export const addCommuterToDatabase = () => {
     name: user.displayName,
     email: user.email,
   })
+  addToUniteDB(user.uid, "user")
 }
