2c0325292b4e0a1f054a125aa33b445c0b803d8f
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-10 10:09:08 +0530

    landing bage bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@
 import { Button } from "./Button"
 export const Hero = () => {
   return (
-    <div className="flex h-screen w-full flex-col px-5 pt-16 sm:px-[6%]">
+    <div className="flex h-screen w-full flex-col bg-[url(/bgmobile.jpg)] bg-cover bg-right px-5 pt-16 sm:bg-[url(/bgdesktop.png)] sm:px-[6%]">
       {/* <h1 className="my-10 text-center font-quicksand text-5xl font-bold text-black sm:text-5xl md:text-5xl lg:text-6xl">
         <span className="mr-2 font-lato sc0:max-sm:text-4xl">Welcome to </span>
         <span className="inline-block first-letter:text-accent">
@@ -14,7 +14,7 @@ export const Hero = () => {
         The solution to your commuting problems
       </h1>
 
-      <p className=" mt-5 w-full text-lg text-zinc-500 sm:mt-4 sm:text-xl md:mt-8 md:text-2xl">
+      <p className=" mt-5 w-full text-lg text-zinc-800 sm:mt-4 sm:text-xl md:mt-8 md:text-2xl">
         Stay on track with our live transit tracker.
       </p>
       {/* Whether you&#39;re a daily
@@ -26,11 +26,12 @@ export const Hero = () => {
         Our user-friendly interface and robust tracking system make it easier
         than ever to plan your journey and minimize waiting times.
       </p> */}
-      <figure className="right-10 flex h-[calc(100%-64px)] w-1/3 max-w-[280px] items-center md:absolute lg:right-14">
-        <img className="" src="trainroutes.png" alt="" />
-      </figure>
 
-      <div className="flex md:mt-24">
+      {/* <figure className="right-10 hidden h-[calc(100%-64px)] w-1/3 max-w-[280px] items-center md:absolute  md:flex lg:right-14">
+        <img className="" src="trainroutes.png" alt="" />
+      </figure> */}
+
+      <div className="mt-32 flex md:mt-24">
         <img className="mr-8 h-20 w-20" src="./icons8-bus-64.png" alt="" />
         <img className="mr-5 h-20 w-20" src="./icons8-train-64.png" alt="" />
       </div>
