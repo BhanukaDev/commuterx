95cbb2caa83365f9be0f467413be9d4bd82f5a34
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-12 18:02:29 +0530

    changes to home page and navbar scroll added for small screens

--- src/pages/HomePage/HomeInformation.jsx
+++ src/pages/HomePage/HomeInformation.jsx
@@ -7,8 +7,8 @@ export const HomeInformation = () => {
     <div>
       {/* container for rest of the parts in homepage */}
       <div className=" h-[91vh] px-5 py-10 md:px-[6%] ">
-        <div className="relative z-10 mb-8 flex flex-col md:mb-0">
-          <h2 className="mb-8 font-manrope text-[clamp(24px,4vw,40px)] ">
+        <div className="relative z-10  flex flex-col ">
+          <h2 className="mb-6 font-manrope text-[clamp(24px,4vw,40px)] ">
             Choose your mode of transport
           </h2>
           <div className="">
@@ -43,7 +43,7 @@ export const HomeInformation = () => {
           </div>
         </div>
 
-        <div className="relative z-0 flex flex-col md:bottom-28 md:items-end ">
+        <div className="relative z-0 mb-6 flex flex-col md:bottom-28 md:items-end ">
           <h2 className="mb-6 font-manrope text-[clamp(24px,4vw,40px)] ">
             Search for your route
           </h2>
@@ -53,8 +53,8 @@ export const HomeInformation = () => {
         </div>
 
         <div className="relative flex flex-col md:bottom-28 ">
-          <h2 className="mb-8 font-manrope text-[clamp(24px,4vw,40px)] ">
-            Something will come here later
+          <h2 className="mb-4 font-manrope text-[clamp(24px,4vw,40px)] ">
+            Something will come here
           </h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
