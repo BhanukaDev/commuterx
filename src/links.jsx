9b7ba379996cdc4e359f6eac689769fbcebddaad
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-31 21:14:55 +0530

    lets check changes

--- src/links.jsx
+++ src/links.jsx
@@ -3,33 +3,33 @@ import { IoMdTrain } from "react-icons/io"
 import { IoBus } from "react-icons/io5"
 
 export const mainlinks = [
-  {
-    id: 1,
-    url: "/features",
-    text: "Features",
-    rfcname: "dd",
-    iconSvg: (
-      <AiFillAlert className="h-full w-full opacity-80 hover:opacity-100" />
-    ),
-  },
-  {
-    id: 2,
-    url: "/support",
-    text: "Support",
-    rfcname: "dd",
-    iconSvg: (
-      <AiFillAliwangwang className="h-full w-full opacity-80 hover:opacity-100" />
-    ),
-  },
-  {
-    id: 3,
-    url: "/aboutus",
-    text: "About Us",
-    rfcname: "dd",
-    iconSvg: (
-      <AiFillBug className="h-full w-full p-[1px] opacity-80 hover:opacity-100" />
-    ),
-  },
+  // {
+  //   id: 1,
+  //   url: "/features",
+  //   text: "Features",
+  //   rfcname: "dd",
+  //   iconSvg: (
+  //     <AiFillAlert className="h-full w-full opacity-80 hover:opacity-100" />
+  //   ),
+  // },
+  // {
+  //   id: 2,
+  //   url: "/support",
+  //   text: "Support",
+  //   rfcname: "dd",
+  //   iconSvg: (
+  //     <AiFillAliwangwang className="h-full w-full opacity-80 hover:opacity-100" />
+  //   ),
+  // },
+  // {
+  //   id: 3,
+  //   url: "/aboutus",
+  //   text: "About Us",
+  //   rfcname: "dd",
+  //   iconSvg: (
+  //     <AiFillBug className="h-full w-full p-[1px] opacity-80 hover:opacity-100" />
+  //   ),
+  // },
 ]
 
 export const transportLinks = [
